decf2e5eff788429dc301789d5d6f67e7e36c68e
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-05-15 01:02:45 +0530

    Complete exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,21 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "8888-3333-444" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
+  const [filterString, setFilterString] = useState("");
 
   const handleNewNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleFilterStringChange = (e) => {
+    setFilterString(e.target.value);
   };
 
   const handleNewNumChange = (e) => {
@@ -28,6 +36,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with{" "}
+      <input value={filterString} onChange={handleFilterStringChange} />
+      <h2>Add a new number</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={handleNewNameChange} />
@@ -42,7 +53,14 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => {
-          return (
+          const personNameUpper = person.name.toUpperCase();
+          return filterString ? (
+            personNameUpper.includes(filterString.toUpperCase()) && (
+              <li key={person.name}>
+                {person.name} {person.number}
+              </li>
+            )
+          ) : (
             <li key={person.name}>
               {person.name} {person.number}
             </li>
